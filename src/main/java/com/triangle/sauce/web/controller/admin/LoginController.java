b9c5e5f4f389150046b0be2e147c0e20ce702b76
ryosuke <ry0suke.uch1tate@gmail.com>
2017-03-17 11:35:10 +0900

    adminログイン

---
+++ src/main/java/com/triangle/sauce/web/controller/admin/LoginController.java
@@ -0,0 +1,25 @@
+package com.triangle.sauce.web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/_admin")
+public class LoginController {
+	@GetMapping("/login")
+	public String index() {
+		return "admin/login";
+	}
+
+	@PostMapping("/login")
+	public String login() {
+		return "redirect:/_admin/dashboard";
+	}
+
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "admin/dashboard";
+	}
+}
